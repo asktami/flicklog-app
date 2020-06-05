5a999f999ccdad06ce9e93c1f79f5c6dae487ec4
Tami Williams <tami@asktami.com>
2020-06-05 11:04:42 -0400

    fixed unused import { Component } warning

--- src/components/MovieListItem/MovieListItem.js
+++ src/components/MovieListItem/MovieListItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import WatchListButton from '../../components/WatchListButton/WatchListButton';
