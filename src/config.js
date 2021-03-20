445a541096ee97099917d3f697cfa302942e0eea
Tami Williams <tami@asktami.com>
2021-03-20 19:58:48 -0400

    removed my cors-anywhere proxy

--- src/config.js
+++ src/config.js
@@ -1,14 +1,11 @@
 // production = hosted on Heroku using PostgreSQL db
 // development = hosted locally using PostgreSQL db
 
-// using my cors-anywhere (DID work)
-// https://asktami-cors-anywhere.herokuapp.com/
-
 const prod = {
 	DATASOURCE: `postgresql`,
 	API_KEY: process.env.REACT_APP_API_KEY,
-	AUTH_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-flicklog-api.herokuapp.com/api`,
-	API_ENDPOINT: `https://asktami-cors-anywhere.herokuapp.com/https://asktami-flicklog-api.herokuapp.com/api`,
+	AUTH_ENDPOINT: `https://asktami-flicklog-api.herokuapp.com/api`,
+	API_ENDPOINT: `https://asktami-flicklog-api.herokuapp.com/api`,
 	EXTERNAL_ENDPOINT: `https://api.themoviedb.org/3`,
 };
 
